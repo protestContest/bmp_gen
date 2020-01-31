60eb8aa7e6bc287365599257d67b834193f8a734
Zack Michener <z@zjm.me>
2020-01-31 12:48:22 -0800

    add support for 2 colors

--- lib/bmp_gen.ex
+++ lib/bmp_gen.ex
@@ -9,16 +9,16 @@ defmodule BmpGen do
     image
   end
 
-  def convert(infile, outfile, method) do
+  def convert(infile, outfile, method, num_colors) do
     read_file(infile)
-    |> convert_pixels(method)
+    |> convert_pixels(method, num_colors)
     |> write_file(outfile)
   end
 
-  defp convert_pixels(image, method) do
+  defp convert_pixels(image, method, num_colors) do
     image.pixels
     |> grayscale(image.bit_depth)
-    |> dither(method)
+    |> dither(method, num_colors)
     |> pack()
   end
 
@@ -42,39 +42,39 @@ defmodule BmpGen do
   defp strip_alpha({ r, g, b, _a}), do: { r, g, b }
   defp strip_alpha(pixel), do: pixel
 
-  defp dither(pixels, method) do
+  defp dither(pixels, method, num_colors) do
     case method do
-      "threshhold" -> threshhold(pixels)
-      _ -> floyd_steinberg(pixels)
+      "threshhold" -> threshhold(pixels, num_colors)
+      _ -> floyd_steinberg(pixels, num_colors)
     end
   end
 
-  defp threshhold(pixels) do
-    IO.puts("Dithering")
+  defp threshhold(pixels, num_colors) do
+    IO.puts("Dithering (threshhold)")
     Enum.map(pixels, fn pixel_row ->
       Enum.map(pixel_row, fn pixel ->
-        floor(pixel * 4)
+        floor(pixel * num_colors)/num_colors
       end)
     end)
   end
 
-  defp floyd_steinberg(pixels) do
-    IO.puts("Dithering")
+  defp floyd_steinberg(pixels, num_colors) do
+    IO.puts("Dithering (floyd-steinberg)")
     row_width = Enum.count(hd pixels)
 
     { dithered_pixels, _errors } = Enum.reduce(pixels, { [], List.duplicate(0, row_width + 1) }, fn pixel_row, { outrows, row_error } ->
-      { new_row, next_row_errors } = dither_row(pixel_row, row_error)
+      { new_row, next_row_errors } = fs_row(pixel_row, row_error, num_colors)
       { outrows ++ [new_row], next_row_errors }
     end)
 
     dithered_pixels
   end
 
-  defp dither_row(pixel_row, row_error) do
+  defp fs_row(pixel_row, row_error, num_colors) do
     row_width = Enum.count(pixel_row)
 
     { new_pixel_row, next_row_errors } = Enum.reduce(pixel_row, { [], row_error }, fn pixel, { outpixels, [ pixel_error | errors ] } ->
-      new_pixel = floor((pixel + pixel_error) * 4)/4
+      new_pixel = floor((pixel + pixel_error) * num_colors)/num_colors
       new_error = pixel - new_pixel
 
       new_errors = errors ++ [0]
@@ -94,7 +94,9 @@ defmodule BmpGen do
     Enum.reduce(pixels, "", fn pixel_row, bytes ->
       row_pairs = Enum.chunk_every(pixel_row, 2)
       Enum.reduce(row_pairs, "", fn [ first | [ last ] ], row_bytes ->
-        row_bytes <> <<first::4, last::4>>
+        first_index = floor(first*4)
+        last_index = floor(last*4)
+        row_bytes <> <<first_index::4, last_index::4>>
       end) <> bytes
     end)
   end
--- lib/bmp_gen/cli.ex
+++ lib/bmp_gen/cli.ex
@@ -1,8 +1,19 @@
 defmodule BmpGen.CLI do
   def main(args) do
-    options = [switches: [in: :string, out: :string, method: :string], aliases: [i: :in, o: :out, m: :method]]
+    options = [
+      switches: [
+        in: :string,
+        out: :string,
+        method: :string,
+        colors: :number
+      ],
+      aliases: [i: :in, o: :out, m: :method, c: :colors]
+    ]
+
     { opts, _, _ } = OptionParser.parse(args, options)
 
-    BmpGen.convert(opts[:in], opts[:out], opts[:method])
+    num_colors = if is_nil(opts[:colors]), do: 4, else: String.to_integer(opts[:colors])
+
+    BmpGen.convert(opts[:in], opts[:out], opts[:method], num_colors)
   end
 end
